96927f30baf5c08e5c3dac38517858b50fc72e8d
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-08-20 17:37:18 +0100

    solve function when pressing = button

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function display(val) {
 function clearScreen() {
   document.getElementById("result").value = "";
 }
+
+function solve() {
+  let x = document.getElementById("result").value;
+
+  let y = eval(x);
+
+  document.getElementById("result").value = y;
+
+  return y;
+}
